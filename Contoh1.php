1541516b585bf1a1842d32909e38a0408bb52fb5
awalliayni <yunipalupy@gmail.com>
2021-09-18 18:18:03 +0700

    first commit

---
+++ Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Dimas Dharma Yudistira Saya tinggal di daerah Kuala dua Olah raga yang saya sukai sepak bola";
+    }
+}
---
+++ dwikcontoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Dwi Kurniawati saya tinggal di daerah Kuala Dua Hobi saya Nonton Drama Korea";
+    }
+}
